a6cbde2f69de50b61fb5559b01a3fc3c4bd83649
Zihadul Islam <zynscriptofficial@gmail.com>
2023-10-29 08:52:58 +0600

    Google Authentication with NextAuth.js Done

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,5 @@
+import {connectMongoDB} from "@/lib/mongodb";
+import User from "@/models/user";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -8,6 +10,37 @@ const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({user, account}) {
+      console.log("User", user);
+      console.log("Account", account);
+      if (account.provider === "google") {
+        const {name, email} = user;
+        try {
+          await connectMongoDB();
+          const userExist = await User.findOne({email});
+          if (!userExist) {
+            const res = await fetch("http://localhost:3000/api/user", {
+              method: "POST",
+              header: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                name,
+                email,
+              }),
+            });
+            if (res.ok) {
+              return user;
+            }
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      return user;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
